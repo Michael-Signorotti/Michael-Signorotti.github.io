8aca4ccbe4c679f4f9649af37a387d0b9b1bb20a
Michael Signorotti <michael.signorotti@gmail.com>
2019-01-16 20:52:27 -0800

    Added typed.js to the header section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
     $('.header').height($(window).height());
-})
+});
+
+$(".navbar a").click(function () {
+    $("body,html").animate({
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
+
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() == 0) {
+        $('.navbar').removeClass('navbar-scrolling')
+        $('.navbar-brand').removeClass('navbar-brand-scrolling')
+        $('.nav-link').removeClass('nav-link-scrolling')
+    } else {
+        $('.navbar').addClass('navbar-scrolling')
+        $('.navbar-brand').addClass('navbar-brand-scrolling')
+        $('.nav-link').addClass('nav-link-scrolling')
+    }
+});
