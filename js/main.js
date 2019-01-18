e8a7f5c586c743e2fa17ef9a1d41b1b122708eea
Michael Signorotti <michael.signorotti@gmail.com>
2019-01-17 23:52:43 -0800

    Added typed.min.js and scroll indicator

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $('.header').height($(window).height());
+    
+    $('#scroll-indicator').addClass('faa-bounce animated faa-slow');
 });
 
 $(".navbar a").click(function () {
@@ -11,12 +13,14 @@ $(".navbar a").click(function () {
 
 $(window).scroll(function () {
     if ($(this).scrollTop() == 0) {
-        $('.navbar').removeClass('navbar-scrolling')
-        $('.navbar-brand').removeClass('navbar-brand-scrolling')
-        $('.nav-link').removeClass('nav-link-scrolling')
+        $('.navbar').removeClass('navbar-scrolling');
+        $('.navbar-brand').removeClass('navbar-brand-scrolling');
+        $('.nav-link').removeClass('nav-link-scrolling');
     } else {
-        $('.navbar').addClass('navbar-scrolling')
-        $('.navbar-brand').addClass('navbar-brand-scrolling')
-        $('.nav-link').addClass('nav-link-scrolling')
+        $('.navbar').addClass('navbar-scrolling');
+        $('.navbar-brand').addClass('navbar-brand-scrolling');
+        $('.nav-link').addClass('nav-link-scrolling');
+        $('#scroll-indicator').removeClass('faa-bounce animated faa-slow');
+        $('#scroll-indicator').hide();
     }
 });
